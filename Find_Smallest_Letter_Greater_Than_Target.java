14dace5867d3e58ddcaaff04f75b59d19da81c34
THE_K1902 <kanadb@iitbhilai.ac.in>
2025-08-03 23:59:21 +0530

    Find Smallest Letter Greater Than Target

---
+++ Find_Smallest_Letter_Greater_Than_Target.java
@@ -0,0 +1,21 @@
+package Leetcode_sols;
+
+public class Find_Smallest_Letter_Greater_Than_Target {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int start=0;
+        int end=letters.length-1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+
+            if(target<letters[mid]){
+
+                end = mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+
+        }
+        return letters[start % letters.length];
+    }
+}
