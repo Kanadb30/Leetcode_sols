a8e67b9e04d7840c9f634ccde4ae03fde4368af0
THE_K1902 <kanadb@iitbhilai.ac.in>
2025-08-04 23:01:57 +0530

    Peak Index in a Mountain Array

---
+++ Peak_Index_in_a_Mountain_Array.java
@@ -0,0 +1,18 @@
+package Leetcode_sols;
+
+public class Peak_Index_in_a_Mountain_Array {
+    public int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length-1;
+        while(start!=end){
+            int mid = start + (end - start)/2;
+            if(arr[mid]>arr[mid+1]){
+                end = mid;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+}
