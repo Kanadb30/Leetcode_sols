71c460a9a56e5bbabc2a3a8367f44a8cfb2a2e9f
THE_K1902 <kanadb@iitbhilai.ac.in>
2025-08-06 23:43:22 +0530

    binary search

---
+++ split_array_largest_sum.java
@@ -0,0 +1,33 @@
+package Leetcode_sols;
+
+public class split_array_largest_sum {
+//  **TIME LIMIT EXCEEDED ERROR**
+//    public int splitArray(int[] nums, int k) {
+//        int start = 0;
+//        int end = 0;
+//        for(int num : nums){
+//            start = Math.max(start,num);
+//            end += num;
+//        }
+//        while(start <= end){
+//            int mid = start + (end - start)/2;
+//            int sum = 0;
+//            int pieces = 1;
+//            for(int num : nums){
+//                if(sum + num > mid){
+//                    sum = num;
+//                    pieces++;
+//                }else{
+//                    sum += num;
+//                }
+//            }
+//            if(pieces > k){
+//                start = mid + 1;
+//            }else{
+//                end = mid;
+//            }
+//
+//        }
+//        return end;
+//    }
+}
