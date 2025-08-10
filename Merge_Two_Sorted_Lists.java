c6d20f043a2b893b0032a1cf528a837e837a15c4
THE_K1902 <kanadb@iitbhilai.ac.in>
2025-08-10 17:28:16 +0530

    Merge Two Sorted Lists

---
+++ Merge_Two_Sorted_Lists.java
@@ -0,0 +1,38 @@
+package Leetcode_sols;
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+public class Merge_Two_Sorted_Lists {
+//    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+//        ListNode ans = new ListNode();
+//        ListNode temp = ans;
+//        while(list1 != null && list2 != null){
+//            if(list1.val <= list2.val){
+//                temp.next = list1;
+//                temp = temp.next;
+//                list1 = list1.next;
+//            }
+//            else {
+//                temp.next = list2;
+//                temp = temp.next;
+//                list2 = list2.next;
+//            }
+//        }
+//        if(list2 != null){
+//            temp.next = list2;
+//        }else if(list1 != null){
+//            temp.next = list1;
+//        }
+//        ans = ans.next;
+//        return ans;
+//
+//    }
+}
