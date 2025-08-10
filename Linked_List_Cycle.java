274e84ec65020ae42f90f87ef1cf596403b44ab0
THE_K1902 <kanadb@iitbhilai.ac.in>
2025-08-10 19:18:15 +0530

    Linked Lists cycle

---
+++ Linked_List_Cycle.java
@@ -0,0 +1,34 @@
+package Leetcode_sols;
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+public class Linked_List_Cycle {
+//    public boolean hasCycle(ListNode head) {
+//        ListNode fast = head;
+//        ListNode slow = head;
+//        if(head == null || head.next == null){
+//            return false;
+//        }
+//        while(fast != null){
+//            slow = slow.next;
+//            fast = fast.next;
+//            if(fast == null){
+//                return false;
+//            }
+//            fast = fast.next;
+//            if(slow == fast){
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+}
