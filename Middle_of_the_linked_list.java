ae242f62015890dca5b9ab46130615f5f4416435
THE_K1902 <kanadb@iitbhilai.ac.in>
2025-08-11 23:48:25 +0530

    Middle of the Linked List

---
+++ Middle_of_the_linked_list.java
@@ -0,0 +1,18 @@
+package Leetcode_sols;
+
+public class Middle_of_the_linked_list {
+    public ListNode middleNode(ListNode head) {
+        int size = 0;
+        ListNode temp = head;
+        while(temp != null){
+            temp = temp.next;
+            size++;
+        }
+        int mid = size/2 + 1;
+        for(int i = 1;i < mid;i++){
+
+            head = head.next;
+        }
+        return head;
+    }
+}
