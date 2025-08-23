f7e8a769a2cd538ebeeb3cd8444a78b6e5bd5c79
THE_K1902 <kanadb@iitbhilai.ac.in>
2025-08-23 16:56:00 +0530

    509. Fibonacci Number

---
+++ Recursion/Fibonacci_Number.java
@@ -0,0 +1,10 @@
+package Leetcode_sols.Recursion;
+
+public class Fibonacci_Number {
+    public int fib(int n) {
+        if(n < 2){
+            return n;
+        }
+        return fib(n-1) + fib(n-2);
+    }
+}
